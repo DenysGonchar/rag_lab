c85090c6857d10f289d2b330c83926376bc60eac
Chris <cbeck@bitcrowd.net>
2025-05-05 23:12:47 +0200

    add tidewave

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule Exmeralda.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:tidewave, "~> 0.1"},
       {:phoenix, "~> 1.7.20"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.10"},
